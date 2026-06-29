90e17f9beb84ca71e1a1144f014a6441db60e682
Mst-Jui <contact.juiakter@gmail.com>
2026-06-29 10:49:44 +0600

    proxy

--- src/proxy.js
+++ src/proxy.js
@@ -1,22 +1,28 @@
 import { NextResponse } from "next/server";
-import { headers } from "next/headers"; 
+import { headers } from "next/headers";
 import { auth } from "./lib/auth";
 
 export async function proxy(request) {
    const session = await auth.api.getSession({
-    headers: await headers()
-   }) 
+      headers: await headers()
+   })
 
-//    if(session?.user?.role == "user" && session?.user?.isPremium === false) {
-//     return NextResponse.redirect(new URL('/pricing', request.url))
-//    }
+   //    if(session?.user?.role == "user" && session?.user?.isPremium === false) {
+   //     return NextResponse.redirect(new URL('/pricing', request.url))
+   //    }
 
-   if(!session){
-    return NextResponse.redirect(new URL('/signin', request.url))
+   if (!session) {
+      return NextResponse.redirect(new URL('/signin', request.url))
    }
 
 }
 
 export const config = {
-    matcher: ['/lessons/:path','/pricing', '/dashboard/user', '/dashboard/admin', '/dashboard/user/add-lessons', '/dashboard/user/my-lessons','/dashboard/user/my-favorites', '/pricing/success', '/pricing/cancel']
+   matcher: [
+      '/lessons/:path*',
+      '/pricing',
+      '/pricing/success',
+      '/pricing/cancel',
+      '/dashboard/:path*',
+   ]
 }
